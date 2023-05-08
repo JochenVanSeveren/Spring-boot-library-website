96c2f6504a1b9579a4d9ab774a50aa39651e20d2
Jochen Van Severen <jochenvanseveren@outlook.com>
2023-05-08 22:29:36 +0200

    Test Gitkraken

--- src/test/java/domain/BookTest.java
+++ src/test/java/domain/BookTest.java
@@ -11,6 +11,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 public class BookTest {
 
     private Validator validator;
